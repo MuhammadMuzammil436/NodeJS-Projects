bb06044cef5a43192e7741ecf291817f4dd5077e
Muhammad Muzammil <159610554+MuhammadMuzammil436@users.noreply.github.com>
2026-02-03 19:57:00 +0500

    Delete school/backend directory

--- school/backend/index.js
+++
@@ -1,5 +0,0 @@
-import { app } from './src/app.js'
-import dotenv from 'dotenv';
-dotenv.config();
-
-app.listen(process.env.PORT, () => console.log('server is running!'));
--- school/backend/src/app.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { GetStudents } from './controllers/StudentsController.js';
-const app = express();
-
-app.get('/students/:id', GetStudents);
-export { app }
